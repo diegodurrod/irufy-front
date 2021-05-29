e076c7c2b05cd834527c06c7f63be96862e7a328
Diego Durán <diego.duran.rguez@gmail.com>
2021-05-29 23:44:34 +0200

    Storybook with Webpack5 successfully installed

---
+++ src/stories/HelloWorld.stories.js
@@ -0,0 +1,23 @@
+import HelloWorld from '../components/HelloWorld.vue';
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: 'HelloWorld',
+  component: HelloWorld,
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => ({
+  components: { HelloWorld },
+  setup() {
+    //👇 The args will now be passed down to the template
+    return { args };
+  },
+  template: '<hello-world v-bind="args" />',
+});
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {
+  /* 👇 The args you need here will depend on your component */
+};
