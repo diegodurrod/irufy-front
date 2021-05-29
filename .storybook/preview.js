5c60bfef23b9116e918c41cd0610911caee89013
Diego Durán <diego.duran.rguez@gmail.com>
2021-05-29 23:44:21 +0200

    Storybook with Webpack5 successfully installed

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,23 @@
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+import {Quasar} from 'quasar';
+import iconSet from 'quasar/icon-set/mdi-v4.js';
+import lang from 'quasar/lang/de.js';
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
+import '@quasar/extras/mdi-v4/mdi-v4.css';
+import 'quasar/dist/quasar.css';
+import { app } from '@storybook/vue3';
+app.use(Quasar, {
+  config: {},
+  plugins: {},
+  lang: lang,
+  iconSet: iconSet
+});
